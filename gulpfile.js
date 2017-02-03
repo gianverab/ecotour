94d39e5c762fe36744df5a0d7e94cb59706fc4e3
Giancarlo Vera Burgos <Giancarlo Vera Burgos>
2017-02-03 00:51:58 -0300

    Browsersync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 //Include Dependencies
-
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 autoprefixer = require('gulp-autoprefixer'),
 concatcss = require('gulp-concat-css'),
-cssnano = require('gulp-cssnano'),
 rename = require('gulp-rename'),
-sourcemaps = require('gulp-sourcemaps');
+sourcemaps = require('gulp-sourcemaps'),
+browsersync = require('browser-sync').create();
 
 //Include Paths
 var paths = {
@@ -16,8 +15,7 @@ var paths = {
 	build: './build/'
 }
 
-//Gulp Tasks
-
+//CSS Workflow
 gulp.task('styles', function(){
 	return gulp.src(paths.src + 'css/**/*.css')
 		.pipe(sourcemaps.init())
@@ -27,14 +25,25 @@ gulp.task('styles', function(){
 				cascade: false
 			}))
 		.pipe(concatcss('all.css'))
-		.pipe(cssnano())		
 		.pipe(rename('app.min.css'))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(paths.build + 'css/'));
 });
 
-gulp.task('serve', ['styles'], function() {
+//HTML Workflow
+gulp.task('html', function(){
+	return gulp.src(paths.src + '*.html')
+	    .pipe(gulp.dest(paths.build));
+});
+
+//Server set up and reload
+gulp.task('serve', ['styles', 'html'], function (){
+	browsersync.init({
+		server: paths.build
+	});
 	gulp.watch(paths.src + 'css/**/*.css', ['styles']); 
+	gulp.watch(paths.src + '*.html', ['html']);
+	gulp.watch(paths.build + '*.html').on('change', browsersync.reload);
 });
 
 //Default gulp command
